fc3b10f6c7b51b1b7c8d8a8b5980313f7d90788f
envy23hp <k_mergul@mail.ru>
2020-01-30 19:40:49 +0600

    One version

---
+++ function.py
@@ -0,0 +1,19 @@
+def discounted(price, discount, max_discount=50):
+    price = abs(float(price))
+    discount = abs(float(discount))
+    max_discount = abs(float(max_discount))
+    if max_discount > 99:
+        raise ValueError('Максимальная скидка не может быть больше 99%')
+    if discount >= max_discount:
+        price_with_discount = price
+    else:
+        price_with_discount = price - price * discount / 100
+    return price_with_discount
+
+
+product = {'name': 'хлеб Заводской', 'price': 80.0, 'discount': 3}
+
+product['with_discount'] = discounted(product['price'], product['discount'], max_discount=10)
+
+
+print(product)
---
+++ hello.py
@@ -0,0 +1,13 @@
+user = 'Python'
+c = 'Hello {name}'.format(name=user)
+print(c)
+
+
+name = 'daulet'
+c = f'hello {name}!'.capitalize()
+print(c)
+
+a = 'H3llo t3b3'
+b = a.replace('3', 'e')
+print(type(a))
+print(b)
